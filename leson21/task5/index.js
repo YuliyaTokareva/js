0fd25bcea9a6c6e05bb517f2e4ef83c7ebea33ce
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-03-26 14:24:37 +0100

    add

--- leson21/task5/index.js
+++ leson21/task5/index.js
@@ -1,7 +1,7 @@
 export const setTitle = text => {
   const getText = document.querySelector('.title');
   getText.textContent = String(text);
-  console.dir((getText.textContent = String(text)));
+  //console.dir((getText.textContent = String(text)));
   return (getText.textContent = String(text));
 };
-setTitle('Hi!');
+//setTitle('Hi!');
