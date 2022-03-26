485427c8da2334bbdf243606033e5ebdd1c81522
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-03-26 14:40:22 +0100

    add edit

--- leson21/task6/index.js
+++ leson21/task6/index.js
@@ -1,8 +1,8 @@
 export const setButton = buttonText => {
   const getText = document.querySelector('body');
   getText.textContent = buttonText;
-  //getText.innerHTML = buttonText;
+  getText.innerHTML = buttonText;
   //console.dir((getText.innetHTML = buttonText));
-  return getText.textContent;
+  return getText.textContent, getText.innerHTML;
 };
 setButton('<button>button text</button>');
