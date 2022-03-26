e9b56d3a1ee815f7fc62a46cf93602841343cbe6
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-03-26 01:21:09 +0100

    add

--- leson21/task4/index.js
+++ leson21/task4/index.js
@@ -1,20 +1,20 @@
 export const getTitle = () => {
-  console.dir(document.querySelector('.title').textContent);
-  return document.querySelector('.title').textContent;
+  console.dir(document.querySelector('.title'));
+  return document.querySelector('.title');
 };
-getTitle();
-export const getDescription = () => {
-  console.dir(document.querySelector('.about').innerText);
-  return document.querySelector('.about').innerText;
-};
-getDescription();
-export const getPlans = () => {
-  console.dir(document.querySelector('.getPlans').innerHTML);
-  return document.querySelector('.getPlans').innerHTML;
-};
-getPlans();
-export const getGoal = () => {
-  console.dir(document.querySelector('.goal').outerHTML);
-  return document.querySelector('.goal').outerHTML;
-};
-getGoal();
+// getTitle();
+// export const getDescription = () => {
+//   console.dir(document.querySelector('.about').innerText);
+//   return document.querySelector('.about').innerText;
+// };
+// getDescription();
+// export const getPlans = () => {
+//   console.dir(document.querySelector('.getPlans').innerHTML);
+//   return document.querySelector('.getPlans').innerHTML;
+// };
+// getPlans();
+// export const getGoal = () => {
+//   console.dir(document.querySelector('.goal').outerHTML);
+//   return document.querySelector('.goal').outerHTML;
+// };
+// getGoal();
