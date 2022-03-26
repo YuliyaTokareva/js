0873c1845e1be73515eba0815df62b7b0e82ae9a
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-03-26 01:31:25 +0100

    add

--- leson21/task4/index.js
+++ leson21/task4/index.js
@@ -7,21 +7,21 @@ getTitle();
 
 export const getDescription = () => {
   const getDescriptionText = document.querySelector('.about');
-  console.dir(getDescriptionText.innerText);
+  //console.dir(getDescriptionText.innerText);
   return getDescriptionText.innerText;
 };
-//getDescription();
+getDescription();
 
-// export const getPlans = () => {
-//   const getPlansText = document.querySelector('.getPlans');
-//   console.dir(getPlansText.innerHTML);
-//   return getPlansText.innerHTML;
-// };
-// getPlans();
+export const getPlans = () => {
+  const getPlansText = document.querySelector('.getPlans');
+  //console.dir(getPlansText.innerHTML);
+  return getPlansText.innerHTML;
+};
+getPlans();
 
-// export const getGoal = () => {
-//   const getGoal = document.querySelector('.goal');
-//   console.dir(getGoal.outerHTML);
-//   return getGoal.outerHTML;
-// };
-// getGoal();
+export const getGoal = () => {
+  const getGoal = document.querySelector('.goal');
+  //console.dir(getGoal.outerHTML);
+  return getGoal.outerHTML;
+};
+getGoal();
