1c23c999a6860936d2cba21d0bd5f9dae793046c
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-06-22 14:46:18 +0300

    add tasks

---
+++ leson34/task3-todo-list-localStorage/src/scripts/renderer.js
@@ -0,0 +1,28 @@
+//import { createCheckboxElem } from './checkbox.js';
+import { getItem } from './storage.js';
+
+const listElem = document.querySelector('.list');
+export const renderTasks = () => {
+  const tasksList = getItem('tasksList') || [];
+  listElem.innerHTML = '';
+
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done, id }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      listItemElem.setAttribute('data-id', `${id}`);
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      checkbox.classList.add('list__item-checkbox');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
+};
